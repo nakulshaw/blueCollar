73385016cf179f6075a87309616e552ccaf4b146
Nakul Shaw <nakulkelvin@gmail.com>
2026-04-06 23:03:46 +0530

    Prioritize Cloudinary_URL and added logging

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -177,13 +177,14 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 if (process.env.CLOUDINARY_URL) {
-    console.log('Cloudinary configured via CLOUDINARY_URL');
+    console.log('Cloudinary: Using CLOUDINARY_URL configuration.');
 } else {
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
     });
+    console.log('Cloudinary: Using individual credentials. Cloud Name:', process.env.CLOUDINARY_CLOUD_NAME);
 }
 
 const storage = new CloudinaryStorage({
